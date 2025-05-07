68f475e1bfa335f38608fde897fb91f4da2e8d9b
Sujeeth <sujeethkumararjun@gmail.com>
2025-05-07 23:17:53 +0530

    Add SpeedInsights component to DashboardLayout for performance insights

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,17 +1,17 @@
 'use client';
 import React from 'react';
-import { usePathname } from 'next/navigation';
 import DashboardProvider from './provider';
 import WelcomeContainer from './dashboard/_components/WelcomeContainer';
-
+import { SpeedInsights } from '@vercel/speed-insights/next'; // Import SpeedInsights
 
 function DashboardLayout({ children }) {
   return (
     <DashboardProvider>
       <div className="p-10 w-full space-y-6">
-        <WelcomeContainer/>
+        <WelcomeContainer />
         {children}
       </div>
+      <SpeedInsights /> {/* Add SpeedInsights component */}
     </DashboardProvider>
   );
 }
